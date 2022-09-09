a10e4a57f30bb42b6554583f48a7648544fa6dfb
cmj1980 <dagoclmt@gmail.com>
2022-09-09 14:47:19 -0700

    phonebook component complete

--- react-phonebook/phonebook/src/App.js
+++ react-phonebook/phonebook/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PhoneBook from './components/phonebook';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />     
+           <PhoneBook />
       </header>
     </div>
   );
--- react-phonebook/phonebook/src/components/phonebook.js
+++ react-phonebook/phonebook/src/components/phonebook.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
 
 
+
 const ContactForm = (props) => {
     const initialFormValue = {
         id: null,
-        contactFirstname: "",
-        contactLastname: "",
-        phoneNumber: ""
+        contactFirstname: " ",
+        contactLastname: " ",
+        phoneNumber: " "
     };
 
-    const [contact, setContact] = useState(initialFormValue);
+    const [contactState, setContactState] = useState(initialFormValue);
 
     const handleOnChange = (e) => {
-        setContact({
-            ...contact,
+        setContactState({
+            ...contactState,
             [e.target.name]: e.target.value,
         });
     };
 
     const handleOnSubmit = (e) => {
         e.preventDefault();
-        if (!contact.contactFirstname || !contact.contactLastname || !contact.phoneNumber) return;
-        props.addContact(contact);
-        setContact(initialFormValue)
+        if (!contactState.contactFirstname || !contactState.contactLastname || !contactState.phoneNumber) return;
+        props.addContact(contactState);
+        setContactState(initialFormValue)
     };
 
 
@@ -30,21 +31,70 @@ const ContactForm = (props) => {
         <form onSubmit={handleOnSubmit}>
             <label>Firstname:</label>
             <br />
-            <input className="contactFirstname" name="contactFirstname" type="text" value={contact.contactFirstname} onChange={handleOnChange} />
+            <input className="contactFirstname" name="contactFirstname" type="text" value={contactState.contactFirstname} onChange={handleOnChange} />
             <br />
             <label>Lastname:</label>
             <br />
-            <input className="contactLastname" name="contactLastname" type="text" value={contact.contactLastname} onChange={handleOnChange} />
+            <input className="contactLastname" name="contactLastname" type="text" value={contactState.contactLastname} onChange={handleOnChange} />
             <br />
             <label>Phone Number:</label>
             <br />
-            <input className="phoneNumber" name="phoneNumber" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/>
+            <input className="phoneNumber" name="phoneNumber" type="text" value={contactState.phoneNumber} onChange={handleOnChange}/>
             <br />
             <input className="submitButton" type="submit" value="Add Contact"/>
         </form>
     )
 };
 
+const ContactTable = (props) => {
+    const sortedContacts = props.newContacts.sort((a, b) =>
+     a.contactFirstname.localeCompare(b.contactFirstname));
+
+    const displayContacts = sortedContacts.length > 0 ? (
+        sortedContacts.map((contact, index) => (
+            <tr key={index}>
+                <td>{contact.contactFirstname}</td>
+                <td>{contact.contactLastname}</td>
+                <td>{contact.phoneNumber}</td>
+            </tr>
+        ))
+    ) : (
+        <tr>
+            <td colSpan={3}>&nbsp;</td>
+        </tr>
+    );
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Phone Number</th>
+                </tr>
+            </thead>
+            <tbody>{displayContacts}</tbody>
+        </table>
+    );
+}
+
+const PhoneBook = (props) => {
+    const contactObj = [];
+
+    const [newContacts, setNewContacts] = useState(contactObj);
+
+    const addContact = (contacts) => {
+     contacts.id = newContacts.length + 1;
+     setNewContacts([...newContacts, contacts])
+    };
+
+    return (
+        <section>
+            <ContactForm addContact={addContact} />
+            <ContactTable newContacts={newContacts} />
+        </section>
+    )
+
+}
 
 
-
+export default PhoneBook;
